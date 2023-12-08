1d9dc1533a5ce3d7359800850a13183350e0c595
ryanjwin <ryanjwin2020@gmail.com>
2023-12-08 12:24:53 -0700

    Finished with coordinates feature

--- bot.py
+++ bot.py
@@ -24,12 +24,117 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
+    """
+    Event triggered when the bot is ready.
+
+    This function prints a message to the console indicating that the bot has logged in.
+    """
     print(f'We have logged in as {bot.user.name}')
 
 @bot.command(name='hello')
 async def hello(ctx):
+    """
+    Command to greet the bot.
+
+    Parameters:
+    - ctx (Context): The context of the command.
+
+    This function sends a greeting message mentioning the user who invoked the command.
+    """
     await ctx.send(f'Hello to you too! {ctx.author.mention}')
 
+@bot.command(name='help')
+async def help(ctx):
+    """
+    Command to display help information.
+
+    Parameters:
+    - ctx (Context): The context of the command.
+
+    This function sends a message containing information about available commands.
+    """
+    help_message = (
+        "Here are the available commands:\n"
+        "!hello - Greet the bot\n"
+        "!savecoords description, x, y, z - Save coordinates\n"
+        "!coords - List all coordinates\n"
+        "!coords \{query\} - List coordinates matching the description\n"
+        "!help - Display this help message"
+    )
+    await ctx.send(help_message)
+
+@bot.command(name='savecoords')
+async def savecoords(ctx, *args):
+    """
+    Save coordinates to the database.
+
+    Parameters:
+    - description (str): Description of the coordinates.
+    - x (float): X-coordinate.
+    - y (float): Y-coordinate.
+    - z (float): Z-coordinate.
+    """
+    if not args or len(args) < 4:
+        await ctx.send('Please provide the description, x, y, and z coordinates. \n!help for more info')
+        return
+    
+    # Parse the arguments
+    description = args[0]
+    x = args[1]
+    y = args[2]
+    z = args[3]
+
+    # Verify that x, y, and z are floats
+    try:
+        x = float(x)
+        y = float(y)
+        z = float(z)
+    except ValueError:
+        await ctx.send('Invalid coordinates. Please ensure that x, y, and z are valid numbers. No commas! \n!help for more info')
+        return
+
+    # insert into database
+    # Insert into the database
+    cursor.execute('INSERT INTO coordinates (description, x, y, z) VALUES (?, ?, ?, ?)',
+                   (description, x, y, z))
+    conn.commit()
+
+    await ctx.send(f'Coordinates saved: [Description: {description}, X: {x}, Y: {y}, Z: {z}]')
+
+@bot.command(name='coords')
+async def coords(ctx, query):
+    """
+    Command to list coordinates.
+
+    Parameters:
+    - ctx (Context): The context of the command.
+    - query (str): Optional query to filter coordinates by description.
+
+    This function lists all coordinates or filters coordinates based on the provided query.
+    """
+    # No specific query.
+    if not query:
+        cursor.execute('SELECT * FROM coordinates')
+        all_coords = cursor.fetchall()
+        # If there are no coordinates in the database.
+        if not all_coords:
+            await ctx.send('No coordinates found.')
+            return
+        # All coordinates.
+        coords_list = '\n'.join(f"{coord[1]}: X={coord[2]}, Y={coord[3]}, Z={coord[4]}" for coord in all_coords)
+        await ctx.send(f'List of coordinates:\n{coords_list}')
+        return
+    # There is a query.
+    cursor.execute('SELECT * FROM coordinates WHERE description LIKE ?', ('%' + query + '%',))
+    filtered_coords = cursor.fetchall()
+    # Nothing found from the query.
+    if not filtered_coords:
+        await ctx.send(f'No coordinates found matching the description: {query}')
+        return
+    # Return the query.
+    coords_list = '\n'.join(f"{coord[1]}: X={coord[2]}, Y={coord[3]}, Z={coord[4]}" for coord in filtered_coords)
+    await ctx.send(f'List of coordinates matching the description "{query}":\n{coords_list}')
+    return
 
 if __name__ == '__main__':
     try:
