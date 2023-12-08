6861d1eeed966045ea2b5317b156b81d2c8af0de
ryanjwin <ryanjwin2020@gmail.com>
2023-12-08 11:22:46 -0700

    first commit

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+import sqlite3
+
+# Connect to SQLite database
+conn = sqlite3.connect('coordinates.db')
+cursor = conn.cursor()
+
+# Create a table to store coordinates if it doesn't exist
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS coordinates (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT,
+        x REAL,
+        y REAL,
+        z REAL
+    )
+''')
+conn.commit()
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user.name}')
+
+
+if __name__ == '__main__':
+    try:
+        load_dotenv()
+        TOKEN = os.getenv('DISCORD_TOKEN')
+        bot.run(TOKEN)
+    finally:
+        conn.close()
